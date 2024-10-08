6c43ffcca6ced571f5ad929c16a536a2b2ed8e2c
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-10-08 08:56:34 +0200

    Skeleton code project

---
+++ 1.5/Source/VFEMedieval/DefOf/VFEM_DefOf.cs
@@ -0,0 +1,15 @@
+﻿using RimWorld;
+using Verse;
+
+namespace VFEMedieval
+{
+    [DefOf]
+    public static class VFEM_DefOf
+    {
+
+        static VFEM_DefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(VFEM_DefOf));
+        }
+    }
+}
---
+++ 1.5/Source/VFEMedieval/HarmonyPatches/HarmonyPatches.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using RimWorld;
+using RimWorld.Planet;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+
+namespace VFEMedieval
+{
+    [StaticConstructorOnStartup]
+    public static class HarmonyPatches
+    {
+        static HarmonyPatches()
+        {
+            //HarmonyInstance.DEBUG = true;
+            VFEMedieval.harmonyInstance.PatchAll();
+
+           
+        }
+    }
+
+}
