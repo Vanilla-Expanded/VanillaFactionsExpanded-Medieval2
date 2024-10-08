e15cc4588d21c159cef0f4d1c8448c97d56144b6
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-10-08 11:20:51 +0200

    Grapes and wine

---
+++ 1.5/Source/VFEMedieval/Comps/CompIngestedThoughtFromQuality.cs
@@ -0,0 +1,25 @@
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace VFEMedieval
+{
+    public class CompIngestedThoughtFromQuality : ThingComp
+    {
+        private CompProperties_IngestedThoughtFromQuality Props => (CompProperties_IngestedThoughtFromQuality)props;
+
+        public override void PostIngested(Pawn ingester)
+        {
+            base.PostIngested(ingester);
+            if (ingester.needs.mood != null)
+            {
+            
+                int quality = (int)parent.GetComp<CompQuality>().Quality;
+
+            
+                ingester.needs.mood.thoughts.memories.TryGainMemory(Props.ingestedThoughts[quality]);
+
+            }
+        }
+    }
+}
---
+++ 1.5/Source/VFEMedieval/Comps/Properties/CompProperties_IngestedThoughtFromQuality.cs
@@ -0,0 +1,25 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace VFEMedieval
+{
+    public class CompProperties_IngestedThoughtFromQuality : CompProperties
+    {
+        public List<ThoughtDef> ingestedThoughts;
+
+        public CompProperties_IngestedThoughtFromQuality()
+        {
+            compClass = typeof(CompIngestedThoughtFromQuality);
+        }
+
+        public override IEnumerable<string> ConfigErrors(ThingDef parentDef)
+        {
+            // Parent def does not have CompQuality
+            if (!parentDef.HasComp(typeof(CompQuality)))
+                yield return $"{parentDef} does not have CompQuality.";
+
+            
+        }
+    }
+}
