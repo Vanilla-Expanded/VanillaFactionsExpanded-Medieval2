e0ef1a6cd96661a6805c38ad7ef47eceffc60e12
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-01-09 18:05:17 +0100

    Castle walls

---
+++ 1.5/Source/VFEMedieval/HarmonyPatches/GenRecipe_MakeRecipeProducts.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
+
+
+namespace VFEMedieval
+{
+
+
+    /*
+
+        [HarmonyPatch(typeof(GenRecipe))]
+        [HarmonyPatch("MakeRecipeProducts")]
+
+        public static class VFEMedieval_GenRecipe_MakeRecipeProducts_Patch
+        {
+
+            public static IEnumerable<Thing> Postfix(IEnumerable<Thing> values, RecipeDef recipeDef, Pawn worker, IBillGiver billGiver)
+            {
+                List<Thing> resultingList = values.ToList();
+                Building_WorkTable workbench;
+                if ((workbench = billGiver as Building_WorkTable) != null && recipeDef.products != null)
+                {
+                    if (workbench.TryGetComp<CompAffectedByFacilities>()?.LinkedFacilitiesListForReading.Contains(ANVIL_HERE) == true)
+                    {
+
+                        foreach (Thing thing in resultingList)
+                        {
+                            CompQuality compQuality = thing.TryGetComp<CompQuality>();
+                            if (compQuality?.Quality < QualityCategory.Normal)
+                            {
+                                if (Rand.Chance(0.2f))
+                                {
+                                    compQuality.SetQuality(compQuality.Quality + 1, null);
+                                    Messages.Message("VFEM2_ItemImproved".Translate(thing.LabelCap, compQuality.Quality.ToString()), worker, MessageTypeDefOf.PositiveEvent, null, historical: false);
+                                }
+
+                            }
+
+                        }
+                    }
+
+
+
+
+
+
+                }
+
+                return resultingList;
+
+
+
+
+
+
+
+
+
+            }
+
+        }
+
+        */
+
+
+
+}
---
+++ 1.5/Source/VFEMedieval/StatParts/StatPart_Flammability.cs
@@ -0,0 +1,37 @@
+﻿
+using RimWorld;
+using RimWorld.Planet;
+using Verse;
+namespace VFEMedieval
+{
+    public class StatPart_Flammability : StatPart
+    {
+   
+
+        public override void TransformValue(StatRequest req, ref float val)
+        {
+            if (req.HasThing)
+            {
+               
+                if (req.Thing.def == VFEM_DefOf.VFEM2_TimberedWall)
+                {
+                    val += 0.5f;
+                }
+            }
+        }
+
+        public override string ExplanationPart(StatRequest req)
+        {
+            if (req.HasThing)
+            {
+                if (req.Thing.def == VFEM_DefOf.VFEM2_TimberedWall)
+                {
+                    return "VFEM2_StatsReport_Flammability".Translate();
+                }
+            }
+            return null;
+        }
+
+      
+    }
+}
