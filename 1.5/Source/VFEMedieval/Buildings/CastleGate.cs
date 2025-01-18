7347b0c84c04d4b72a1540bb0a04d56d048a3ec8
Taranchuk <nfhfyxer@gmail.com>
2025-01-18 17:01:27 +0200

    castle gates

---
+++ 1.5/Source/VFEMedieval/Buildings/CastleGate.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace VFEMedieval
+{
+    [HotSwappable]
+    public class CastleGate : Building_MultiTileDoor
+    {
+        protected int interval;
+
+        public override void Tick()
+        {
+            base.Tick();
+            if (openInt is false)
+            {
+                for (var i = 0; i < 9; i++)
+                {
+                    if (ticksSinceOpen > 0)
+                    {
+                        ticksSinceOpen--;
+                    }
+                }
+            }
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref interval, "interval");
+        }
+    }
+}
