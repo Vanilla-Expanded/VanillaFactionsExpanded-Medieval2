5923989d3f0e3b2e6e686ae9377c6a4ba4bf953a
Taranchuk <nfhfyxer@gmail.com>
2025-01-28 19:58:49 +0200

    merchant caravans tweaks

---
+++ 1.5/Source/VFEMedieval/MerchantGuild/Caravan_PathFollower_StartPath_Patch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using RimWorld.Planet;
+
+namespace VFEMedieval
+{
+    [HarmonyPatch(typeof(Caravan_PathFollower), "StartPath")]
+    public static class Caravan_PathFollower_StartPath_Patch
+    {
+        public static void Postfix(Caravan_PathFollower __instance, int destTile)
+        {
+            if (Caravan_PathFollower_ExposeData_Patch.merchantsToFollow.TryGetValue(__instance, out var merchant)
+                && destTile != merchant.Tile)
+            {
+                Caravan_PathFollower_ExposeData_Patch.merchantsToFollow.Remove(__instance);
+            }
+        }
+    }
+}
