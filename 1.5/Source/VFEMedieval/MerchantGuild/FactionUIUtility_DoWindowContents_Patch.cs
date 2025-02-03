0a9fa66c48831a01a91bd680c00ac02c05f85d10
Taranchuk <nfhfyxer@gmail.com>
2025-02-03 15:48:14 +0200

    make merchant guild faction visible in the UI

---
+++ 1.5/Source/VFEMedieval/MerchantGuild/FactionUIUtility_DoWindowContents_Patch.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace VFEMedieval
+{
+    [HarmonyPatch(typeof(FactionUIUtility), "DoWindowContents")]
+    public static class FactionUIUtility_DoWindowContents_Patch
+    {
+        public static void Prefix()
+        {
+            Faction faction = Find.FactionManager.FirstFactionOfDef(VFEM_DefOf.VFEM2_MerchantGuild);
+            if (faction != null)
+            {
+                faction.hidden = false;
+            }
+        }
+
+        public static void Postfix()
+        {
+            Faction faction = Find.FactionManager.FirstFactionOfDef(VFEM_DefOf.VFEM2_MerchantGuild);
+            if (faction != null)
+            {
+                faction.hidden = true;
+            }
+        }
+    }
+}
