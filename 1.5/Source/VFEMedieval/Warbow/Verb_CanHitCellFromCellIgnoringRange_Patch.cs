802b9960731799539199f41088d12038978a5db5
Taranchuk <nfhfyxer@gmail.com>
2025-02-08 16:25:13 +0200

    warbow targeting tweaks

--- 1.5/Source/VFEMedieval/Warbow/Verb_CanHitCellFromCellIgnoringRange_Patch.cs
+++ 1.5/Source/VFEMedieval/Warbow/Verb_CanHitCellFromCellIgnoringRange_Patch.cs
@@ -10,8 +10,37 @@ namespace VFEMedieval
         {
             if (__instance.verbProps is VerbProperties_Warbow)
             {
-                __result = targetLoc.Roofed(__instance.caster.Map) is false || GenSight.LineOfSight(sourceSq, targetLoc, __instance.caster.Map, skipFirstCell: true);
-                return false;
+                var casterMap = __instance.caster.Map;
+                var casterRoofed = sourceSq.Roofed(casterMap);
+                var targetRoofed = targetLoc.Roofed(casterMap);
+
+                // If the target is in LoS, always allow the shot.
+                if (GenSight.LineOfSight(sourceSq, targetLoc, casterMap, skipFirstCell: true))
+                {
+                    __result = true;
+                    return false;
+                }
+
+                // If both the caster and target are unroofed, allow the shot.
+                if (!casterRoofed && !targetRoofed)
+                {
+                    __result = true;
+                    return false;
+                }
+
+                // If the caster is under a roof, they need LoS to fire.
+                if (casterRoofed)
+                {
+                    __result = false;
+                    return false;
+                }
+
+                // If the target is under a roof while the caster is not, they cannot be hit.
+                if (!casterRoofed && targetRoofed)
+                {
+                    __result = false;
+                    return false;
+                }
             }
             return true;
         }
