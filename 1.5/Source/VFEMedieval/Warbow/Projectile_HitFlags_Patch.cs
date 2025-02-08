5339ca261090d04a32ece75f591ad56fa7efc9d8
Taranchuk <nfhfyxer@gmail.com>
2025-02-08 15:41:51 +0200

    warbow accuracy fix

---
+++ 1.5/Source/VFEMedieval/Warbow/Projectile_HitFlags_Patch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace VFEMedieval
+{
+    [HarmonyPatch(typeof(Projectile), "HitFlags", MethodType.Getter)]
+    public static class Projectile_HitFlags_Patch
+    {
+        public static void Postfix(Projectile __instance, ref ProjectileHitFlags __result)
+        {
+            if (__instance is WarbowArrow && __instance.ticksToImpact <= 10)
+            {
+                __result = ProjectileHitFlags.All;
+            }
+        }
+    }
+
+}
