24e5e46a7674670acf3e3c36bcbe5c9feff27d57
SokyranTheDragon <exploder1531@gmail.com>
2026-01-26 17:07:16 +0100

    (Low) castle walls and their destroyed variants don't need to tick anymore

--- 1.6/Source/VFEMedieval/Buildings/CastleWall.cs
+++ 1.6/Source/VFEMedieval/Buildings/CastleWall.cs
@@ -1,41 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using UnityEngine;
-using Verse;
+﻿using Verse;
 using RimWorld;
-using VFEMedieval;
 
 namespace VFEMedieval
 {
     public class CastleWall : Building
     {
-      
-        public override void TickRare()
+        public override int HitPoints
         {
-            base.TickRare();
-
-            if (this.HitPoints < 0.3 * MaxHitPoints)
+            set
             {
-                // Need to check before spawning a replacement, as it'll destroy (and deselect) the current wall
-                var selected = Find.Selector.IsSelected(this);
+                base.HitPoints = value;
+                Notify_HitPointsChanged(value);
+            }
+        }
 
-                Thing thingToMake = GenSpawn.Spawn(ThingMaker.MakeThing(VFEM_DefOf.VFEM2_DamagedCastleWall, this.Stuff), this.PositionHeld, this.Map);
-                thingToMake.SetFaction(this.Faction);
+        public void Notify_HitPointsChanged(int hitPoints)
+        {
+            if (hitPoints <= 0 || hitPoints >= 0.3 * MaxHitPoints || this.Map == null)
+                return;
 
-                DamageInfo dinfo = new DamageInfo(DamageDefOf.Blunt, thingToMake.HitPoints*0.5f);
-                thingToMake.TakeDamage(dinfo);
+            // Need to check before spawning a replacement, as it'll destroy (and deselect) the current wall
+            var selected = Find.Selector.IsSelected(this);
 
-                if (selected)
-                {
-                    Find.Selector.Select(thingToMake, false, false);
-                }
+            Thing thingToMake = GenSpawn.Spawn(ThingMaker.MakeThing(VFEM_DefOf.VFEM2_DamagedCastleWall, this.Stuff), this.PositionHeld, this.Map);
+            thingToMake.SetFaction(this.Faction);
 
-                if (this.Spawned)
-                {
-                    this.Destroy();
-                }
+            DamageInfo dinfo = new DamageInfo(DamageDefOf.Blunt, thingToMake.HitPoints*0.5f);
+            thingToMake.TakeDamage(dinfo);
 
+            if (selected)
+            {
+                Find.Selector.Select(thingToMake, false, false);
+            }
+
+            if (this.Spawned)
+            {
+                this.Destroy();
             }
         }
     }
--- 1.6/Source/VFEMedieval/Buildings/DamagedCastleWall.cs
+++ 1.6/Source/VFEMedieval/Buildings/DamagedCastleWall.cs
@@ -1,43 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using UnityEngine;
-using Verse;
+﻿using Verse;
 using RimWorld;
-using VFEMedieval;
 
 namespace VFEMedieval
 {
     public class DamagedCastleWall : Building
     {
-
-        public override void TickRare()
+        public override int HitPoints
         {
-            base.TickRare();
-
-            if (this.HitPoints > 0.3 * MaxHitPoints)
+            set
             {
-                // Need to check before spawning a replacement, as it'll destroy (and deselect) the current wall
-                var selected = Find.Selector.IsSelected(this);
-
-                Thing thingToMake = GenSpawn.Spawn(ThingMaker.MakeThing(VFEM_DefOf.VFEM2_CastleWall, this.Stuff), this.PositionHeld, this.Map);
-                thingToMake.SetFaction(this.Faction);
-                DamageInfo dinfo = new DamageInfo(DamageDefOf.Blunt, thingToMake.HitPoints * 0.4f);
-                thingToMake.TakeDamage(dinfo);
-
-                if (selected)
-                {
-                    Find.Selector.Select(thingToMake, false, false);
-                }
-
-                if (this.Spawned)
-                {
-                    this.Destroy();
-                }
-                
-
+                base.HitPoints = value;
+                Notify_HitPointsChanged(value);
             }
         }
-       
+
+        public void Notify_HitPointsChanged(int hitPoints)
+        {
+            if (hitPoints <= 0 || hitPoints <= 0.3 * MaxHitPoints || this.Map == null)
+                return;
+
+            // Need to check before spawning a replacement, as it'll destroy (and deselect) the current wall
+            var selected = Find.Selector.IsSelected(this);
+
+            Thing thingToMake = GenSpawn.Spawn(ThingMaker.MakeThing(VFEM_DefOf.VFEM2_CastleWall, this.Stuff), this.PositionHeld, this.Map);
+            thingToMake.SetFaction(this.Faction);
+            DamageInfo dinfo = new DamageInfo(DamageDefOf.Blunt, thingToMake.HitPoints * 0.4f);
+            thingToMake.TakeDamage(dinfo);
+
+            if (selected)
+            {
+                Find.Selector.Select(thingToMake, false, false);
+            }
+
+            if (this.Spawned)
+            {
+                this.Destroy();
+            }
+        }
     }
 }
